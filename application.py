e1b96d39258076a11d88de9732cb0dc837347e8c
timbohiatt <timbohiatt@gmail.com>
2018-06-13 15:08:26 +0200

    Resolving Deployment Issues. Potential reserved key

--- application.py
+++ application.py
@@ -72,14 +72,14 @@ images = UploadSet('images', IMAGES)
 
 
 
-# ==WRAPPERS===================================================================
-@application.before_request
-def before_request():
-	if (application.config['RunEnv'] is not "local"):
-		if request.url.startswith('http://'):
-			url = request.url.replace('http://', 'https://', 1)
-			code = 301
-			return redirect(url, code=code)
+## ==WRAPPERS===================================================================
+#@application.before_request
+#def before_request():
+#	if (application.config['RunEnv'] is not "local"):
+#		if request.url.startswith('http://'):
+#			url = request.url.replace('http://', 'https://', 1)
+#			code = 301
+#			return redirect(url, code=code)
 
 
 def loginStatus(f):
